ddf941797dcff8a6e84d0e2391d77546ed4890c1
itacirgabral <itacirgabral@@users.noreply.github.com>
2018-09-21 12:28:08 -0400

    cors and ajax

--- httpanatomy.js
+++ httpanatomy.js
@@ -9,7 +9,6 @@ http.createServer((request, response) => {
     body.push(chunk);
   }).on('end', () => {
     body = Buffer.concat(body).toString();
-    // BEGINNING OF NEW STUFF
 
     response.on('error', (err) => {
       console.error(err);
@@ -17,17 +16,13 @@ http.createServer((request, response) => {
 
     response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.writeHead(200, {'Content-Type': 'application/json'})
+    response.setHeader('Access-Control-Allow-Origin', '*')
 
     const responseBody = { headers, method, url, body };
 
     response.write(JSON.stringify(responseBody, null, 4));
     response.end();
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
 
-    // END OF NEW STUFF
   });
 }).listen(8080);
 // https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
